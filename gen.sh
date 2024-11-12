efcd2a8ec54021b8c765d1aa0a819bb46a87bae8
hg-tc <1344405655@qq.com>
2024-11-12 03:04:52 -0800

    add dataset

--- gen.sh
+++ gen.sh
@@ -1,4 +1,4 @@
-./inst_gen 1 mono_test1_output_pos_.txt mono_test1_output_num_.txt mono
-./inst_gen 2 fusion_test1_output_pos_.txt fusion_test1_output_num_.txt fusion
-./inst_gen 3 global_BA_1_output_pos_.txt global_BA_1_output_num_.txt sfm2
-./inst_gen 4 global_BA_4_output_pos_.txt global_BA_4_output_num_.txt sfm3
+./inst_gen 1 Data/mono_test1_output_pos_.txt Data/mono_test1_output_num_.txt mono
+./inst_gen 2 Data/fusion_test1_output_pos_.txt Data/fusion_test1_output_num_.txt fusion
+./inst_gen 3 Data/global_BA_1_output_pos_.txt Data/global_BA_1_output_num_.txt sfm2
+./inst_gen 4 Data/global_BA_4_output_pos_.txt Data/global_BA_4_output_num_.txt sfm3
