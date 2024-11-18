5e8a465334a3dc05c7f2cc716ee713b5a87ecb6a
hg-tc <1344405655@qq.com>
2024-11-18 04:33:01 -0800

    PE_num_Gen

--- gen.sh
+++ gen.sh
@@ -1,4 +1,4 @@
-./inst_gen 1 Data/mono_test1_output_pos_.txt Data/mono_test1_output_num_.txt mono 5
-./inst_gen 2 Data/fusion_test1_output_pos_.txt Data/fusion_test1_output_num_.txt fusion 5
-./inst_gen 3 Data/global_BA_1_output_pos_.txt Data/global_BA_1_output_num_.txt sfm2 5
-./inst_gen 4 Data/global_BA_4_output_pos_.txt Data/global_BA_4_output_num_.txt sfm3 5
+./inst_gen 1 Data/mono_test1_output_pos_.txt Data/mono_test1_output_num_.txt mono 6
+./inst_gen 2 Data/fusion_test1_output_pos_.txt Data/fusion_test1_output_num_.txt fusion 6
+./inst_gen 3 Data/global_BA_1_output_pos_.txt Data/global_BA_1_output_num_.txt sfm2 6
+./inst_gen 4 Data/global_BA_4_output_pos_.txt Data/global_BA_4_output_num_.txt sfm3 6
